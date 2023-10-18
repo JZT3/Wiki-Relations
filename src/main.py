e7bbbd64c6391d3f7778d9eaac076a0249993f29
Jervani Z Thompson <jzthompson20@gmail.com>
2023-10-17 21:04:58 -0700

    fixed threading and improved variable names and added documentaion

--- src/main.py
+++ src/main.py
@@ -1,46 +1,96 @@
 import json
 import mwclient
 import concurrent.futures
+import threading
 from collections import defaultdict, deque
+from typing import List, Dict, Set
 
-def worker(d, site, visited_articles, links_lock, links, depth, visited_lock):
-    while d:
-        article_title, current_depth = d.popleft()
+def fetch_article_links(article_queue: deque, 
+                        site: mwclient.Site, 
+                        visited_articles: Set[str], 
+                        links_lock: threading.Lock, 
+                        article_links: Dict[str, List[str]], 
+                        max_depth: int, 
+                        visited_lock: threading.Lock) -> None:
+    """
+    Worker function to fetch links of articles from Wikipedia.
+    
+    Args:
+    - article_queue: A queue containing articles to be processed.
+    - site: mwclient site object for Wikipedia.
+    - visited_articles: A set to keep track of processed articles.
+    - links_lock: A lock to ensure thread-safe updates to article_links.
+    - article_links: Dictionary storing links for each article.
+    - max_depth: Maximum depth for recursive link fetching.
+    - visited_lock: A lock to ensure thread-safe updates to visited_articles.
+    """
+    
+    while article_queue:
+        try:
+            article_title, current_depth = article_queue.popleft()
+        except IndexError:  # Handles case when deque is empty
+            break
         
+        # Check if article already visited or max depth reached
         with visited_lock:
-            if current_depth > depth or article_title in visited_articles:
+            if current_depth > max_depth or article_title in visited_articles:
                 continue
             visited_articles.add(article_title)
         
+        # Fetch links from the current article
         page = site.pages[article_title]
-
-        if page.namespace == 0:
+        if page.namespace == 0:  # Ensure it's an actual article
             linked_articles = [link.name for link in page.links()]
 
+            # Update the global links dictionary
             with links_lock:
-                links[article_title].extend(linked_articles)
+                article_links[article_title].extend(linked_articles)
 
+            # Add linked articles to the queue for further processing
             for linked_article in linked_articles:
-                d.append((linked_article, current_depth + 1))
+                article_queue.append((linked_article, current_depth + 1))
 
-def collect_links_multithreaded(site, seed_articles, depth=2):
-    links = defaultdict(list)
-    d = deque((article, 1) for article in seed_articles)
+def collect_links_multithreaded(site: mwclient.Site, 
+                               seed_articles: List[str], 
+                               depth: int = 2) -> Dict[str, List[str]]:
+    """
+    Collect links from Wikipedia articles using a multithreaded approach.
+    
+    Args:
+    - site: mwclient site object for Wikipedia.
+    - seed_articles: Initial articles to start the link collection.
+    - depth: Maximum depth for link fetching.
+
+    Returns:
+    - Dictionary with articles as keys and their linked articles as values.
+    """
+    
+    article_links = defaultdict(list)
+    article_queue = deque((article, 1) for article in seed_articles)
     visited_articles = set()
-    links_lock = concurrent.futures.Lock()
-    visited_lock = concurrent.futures.Lock()
+    links_lock = threading.Lock()
+    visited_lock = threading.Lock()
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        futures = [executor.submit(worker, d, site, visited_articles, links_lock, links, depth, visited_lock) for _ in range(10)]
+        # Start worker threads
+        futures = [executor.submit(fetch_article_links, article_queue, site, visited_articles, links_lock, article_links, depth, visited_lock) for _ in range(10)]
         concurrent.futures.wait(futures)
     
-    return links
+    return article_links
 
-def main():
+def main() -> None:
+    """Main function to fetch and save Wikipedia article links."""
+    
+    # Connect to Wikipedia
     site = mwclient.Site('en.wikipedia.org')
+    
+    # Define seed articles
     seed_articles = ['Mathematics', 'Computer_Science']
+    
+    # Fetch links using a multithreaded approach
     links = collect_links_multithreaded(site, seed_articles)
 
+    # Save links to a JSON file
     with open('wikipedia_links.json', 'w') as f:
         json.dump(links, f)
 
