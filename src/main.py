5a055a269d2c5a57c1868beacbbad40296bc7706
Jervani Z Thompson <jzthompson20@gmail.com>
2023-10-17 20:53:06 -0700

    Changed recurssive implementation to asynchronous

--- src/main.py
+++ src/main.py
@@ -1,48 +1,50 @@
 import json
 import mwclient
+import concurrent.futures
+from collections import defaultdict, deque
 
-def collect_links(site, article_title, visited_articles, links, depth=2):
-    """
-    Recursively collect links from a given Wikipedia article up to a specified depth.
-    """
-    if depth == 0 or article_title in visited_articles:
-        return
+def worker(d, site, visited_articles, links_lock, links, depth, visited_lock):
+    while d:
+        article_title, current_depth = d.popleft()
+        
+        with visited_lock:
+            if current_depth > depth or article_title in visited_articles:
+                continue
+            visited_articles.add(article_title)
+        
+        page = site.pages[article_title]
 
-    visited_articles.add(article_title)
-    page = site.pages[article_title]
+        if page.namespace == 0:
+            linked_articles = [link.name for link in page.links()]
 
-    # Only consider pages in the main namespace (i.e., actual articles)
-    if page.namespace == 0:
-        linked_articles = [link.name for link in page.links()]
-        links[article_title] = linked_articles
+            with links_lock:
+                links[article_title].extend(linked_articles)
 
-        # Recursively fetch links from linked articles
-        for linked_article in linked_articles:
-            collect_links(site, linked_article, visited_articles, links, depth-1)
+            for linked_article in linked_articles:
+                d.append((linked_article, current_depth + 1))
 
+def collect_links_multithreaded(site, seed_articles, depth=2):
+    links = defaultdict(list)
+    d = deque((article, 1) for article in seed_articles)
+    visited_articles = set()
+    links_lock = concurrent.futures.Lock()
+    visited_lock = concurrent.futures.Lock()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(worker, d, site, visited_articles, links_lock, links, depth, visited_lock) for _ in range(10)]
+        concurrent.futures.wait(futures)
+    
+    return links
 
 def main():
-    # Connect to Wikipedia
     site = mwclient.Site('en.wikipedia.org')
-
-    # List of initial "seed" articles for math and computer science
     seed_articles = ['Mathematics', 'Computer_Science']
+    links = collect_links_multithreaded(site, seed_articles)
 
-    # Set to store visited articles to avoid loops
-    visited_articles = set()
-
-    # Dictionary to store links between articles
-    links = {}
-
-    # Start data collection from seed articles
-    for seed in seed_articles:
-        collect_links(site, seed, visited_articles, links)
-
-    # Save links to a JSON file
     with open('wikipedia_links.json', 'w') as f:
         json.dump(links, f)
 
-    print(f"Collected data for {len(visited_articles)} articles.")
+    print(f"Collected data for {len(links)} articles.")
 
 if __name__ == "__main__":
     main()
